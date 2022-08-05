5757750e0e243d1eea1997d75b64f3ff97c147f9
CSH9194 <csh9194@gamil.com>
2022-08-05 09:46:11 +0900

    시간 수정

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,8 @@ class Post(models.Model):
     content = models.TextField()
 
 
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
