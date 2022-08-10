07f22017d38df6c5e58ff1d36ad9c3fc624d62e8
CSH9194 <csh9194@gamil.com>
2022-08-10 14:21:50 +0900

    태그 및 태그페이지 생성

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from.models import Post, Category
+from.models import Post, Category, Tag
 
 admin.site.register(Post)
 
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name', )}
-
 admin.site.register(Category, CategoryAdmin)
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )}
+admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,18 @@ import os
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -18,6 +30,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
+
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -33,6 +46,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, blank=True)
 
 
     def __str__(self):
